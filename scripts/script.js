d26ee3c3e6239fa88d27c7f62bcf81ab56a7c862
Kyle Veloso <kyle.veloso@dawsoncollege.qc.ca>
2021-09-24 23:29:06 -0400

    modified js script that creates increaingly taller bars in graph

--- scripts/script.js
+++ scripts/script.js
@@ -12,7 +12,7 @@ function setup() {
     //array size constant
     const chartHeight = 600
     const chartWidth = 1200
-    const size = 50;
+    const size = 25;
     let numbers = [1,2,3,4,5,6,7,8,9]
 
     for (let i = 0; i < numbers.length; i++) {
@@ -32,12 +32,20 @@ function setup() {
     
 
     for (let i = 0; i < size; i++) {
+
+        const allowableItemMaxHeight = chartHeight * 0.9
+        const allowableItemMaxWidth = chartWidth*0.9
         let newListItem = listItemBlueprint.cloneNode()
         numberList.appendChild(newListItem)
         newListItem.setAttribute("id", `n${i}`)
         newListItem.textContent = i
         console.log(newListItem.getAttribute("id"))
-        newListItem.style.width = `${(chartWidth-100)/size}px`
+        newListItem.style.width = `${(allowableItemMaxWidth) / size}px`
+        newListItem.style.height =  `${(i+1)/(size+1) * allowableItemMaxHeight}px`;
     }
 
+}
+
+function getValuesInChart() {
+    
 }
