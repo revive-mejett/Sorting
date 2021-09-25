8d76c3a37f8525d97d42a9917e6c31b33e91b4e4
Kyle Veloso <kyle.veloso@dawsoncollege.qc.ca>
2021-09-24 23:40:13 -0400

    added random id number function

--- scripts/script.js
+++ scripts/script.js
@@ -5,20 +5,18 @@
 document.addEventListener("DOMContentLoaded", setup) 
 
 
+//chart characteristics constants
+const chartHeight = 600
+const chartWidth = 1200
+const size = 15;
 
 function setup() {
 
 
-    //array size constant
-    const chartHeight = 600
-    const chartWidth = 1200
-    const size = 25;
+    
     let numbers = [1,2,3,4,5,6,7,8,9]
 
-    for (let i = 0; i < numbers.length; i++) {
-        console.log(numbers[i])
-    }
-
+ 
     let numberList = document.querySelector(".numbers")
     console.log(numberList)
 
@@ -44,8 +42,26 @@ function setup() {
         newListItem.style.height =  `${(i+1)/(size+1) * allowableItemMaxHeight}px`;
     }
 
+    // for (let i = 0; i < numbers.length; i++) {
+    //     console.log(numbers[i])
+    // }
+    getValuesInChart()
+
+    setTimeout(() => {
+        
+    }, timeout);
+
 }
 
 function getValuesInChart() {
-    
+    const numberList = document.querySelector(".numbers")
+    let allItems = numberList.querySelectorAll(".array-flex-item")
+
+
+    console.log(randItemNumber())
+}
+
+function randItemNumber() {
+    let randomId = Math.floor(Math.random()*size)
+    return randomId
 }
