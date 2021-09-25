c2037824896d025bd3ddd107335df1832cb34250
Kyle Veloso <kyle.veloso@dawsoncollege.qc.ca>
2021-09-24 22:33:07 -0400

    added a few styles, html skeleton and setup function js

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+'use strict'
+
+
+
+document.addEventListener("DOMContentLoaded", setup) 
+
+
+
+function setup() {
+
+    let numbers = [1,2,3,4,5,6,7,8,9]
+
+    for (let i = 0; i < numbers.length; i++) {
+        console.log(numbers[i])
+    }
+
+}
